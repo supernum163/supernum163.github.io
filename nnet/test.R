585465cf60a9edb6944b5f0d7668592544e649c3
13524769385 <13524769385@163.com>
2020-07-03 09:28:31 +0800

    2020年 07月 03日

--- nnet/test.R
+++ nnet/test.R
@@ -7,6 +7,27 @@ source("optimizer.R")
 source("nnet.R")
 
 
+
+# 两层全链接神经网络
+input <- matrix(c(-2, -1, 25, 6, 17, 4, -15, -6), nrow = 4, byrow = TRUE)
+output = array(c(1, 0, 0, 1), dim = c(4, 1))
+
+nnet <- Nnet(dim(input), dim(output))
+LAYER$Affine(nnet, n = 2, "Sigmoid")
+LAYER$Sigmoid(nnet)
+LAYER$Affine(nnet, n = 1, "Sigmoid")
+LAYER$Sigmoid(nnet)
+LAYER$LAST$MeanSquared(nnet)
+OPTIMIZER$Adam(nnet, lr = 0.1)
+nnet$fFeed <- function(nnet) {
+  nnet$data$input0 <- input
+  nnet$data$output <- output
+}
+nnet$train(500)
+plot(nnet$losses)
+
+
+
 # 基于minist数据集，数字识别
 n <- 100
 dim_input <- c(n, 1, 28, 28)
@@ -44,26 +65,6 @@ plot(nnet$losses)
 
 
 
-# 两层全链接神经网络
-input <- matrix(c(-2, -1, 25, 6, 17, 4, -15, -6), nrow = 4, byrow = TRUE)
-output = array(c(1, 0, 0, 1), dim = c(4, 1))
-
-nnet <- Nnet(dim(input), dim(output))
-LAYER$Affine(nnet, n = 2, "Sigmoid")
-LAYER$Sigmoid(nnet)
-LAYER$Affine(nnet, n = 1, "Sigmoid")
-LAYER$Sigmoid(nnet)
-LAYER$LAST$MeanSquared(nnet)
-OPTIMIZER$Adam(nnet, lr = 0.1)
-nnet$fFeed <- function(nnet) {
-  nnet$data$input0 <- input
-  nnet$data$output <- output
-}
-nnet$train(500)
-plot(nnet$losses)
-
-
-
 # 神经网络中的关键信息
 nnet$layers
 nnet$data[nnet$params]
@@ -71,6 +72,8 @@ nnet$data[nnet$Dparams]
 inputs <- ls(nnet$data, pattern = "input\\d+")
 nnet$data[inputs]
 nnet$get("input", nnet$layerId)
+nnet$get("output")
+self <- nnet$layers[[3]]
 input <- nnet$get("input", nnet$layerId - 1)
 
 
